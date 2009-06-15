2dd3c6aca7030932dce97d059538547b00301c05
Danilo Piparo <sha1-e2231637d47316c544de5e218528626d57de224e@cern.ch>
2009-06-15 12:51:56 +0000

    preprocessor statements re-arranged

--- test/m2lnQ_creator.cpp
+++ test/m2lnQ_creator.cpp
@@ -13,17 +13,11 @@ It dumps a png image of the plot and a rootfile with the LimitResults object.
 
 #if (defined (STANDALONE) or defined (__CINT__) )
    #include "LimitCalculator.h"
-#else
-   #include "PhysicsTools/RooStatsCms/interface/LimitCalculator.h"
-#endif
-#if (defined (STANDALONE) or defined (__CINT__) )
    #include "RscCombinedModel.h"
-#else
-   #include "PhysicsTools/RooStatsCms/interface/RscCombinedModel.h"
-#endif
-#if (defined (STANDALONE) or defined (__CINT__) )
    #include "RscConstrArrayFiller.h"
 #else
+   #include "PhysicsTools/RooStatsCms/interface/LimitCalculator.h"
+   #include "PhysicsTools/RooStatsCms/interface/RscCombinedModel.h"
    #include "PhysicsTools/RooStatsCms/interface/RscConstrArrayFiller.h"
 #endif
 
